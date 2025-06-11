6b9019bb14a4363514ba32ce19118b4917268cfc
Russo69max <juanpedrofreitas69@gmail.com>
2025-06-11 11:44:21 -0300

    Update Russo5.py

--- Russo5.py
+++ Russo5.py
@@ -2,6 +2,6 @@ nome = int(input("Me fala um numero qualquer vc vc queira irmao?"))
 nome2 = int(input("Onde voce quer começar?"))
 nome3 = int(input("Onde vc quer terminar?"))
 
-for i in range(nome2,nome3):
+for i in range(nome2,nome3+1):
     print(f"{i} x {nome} = {i * nome}")
 
